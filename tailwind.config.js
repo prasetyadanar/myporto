44994f1229dd65f4cf52dd0acd6b3849365633a1
prasetyadanar <prasetyadanar92@gmail.com>
2024-06-04 13:21:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {},
+    colors: {
+      primary: '#07071F',
+      secondary: '#0A0A22',
+      foot: '#050517',
+      gold: '#F4BE10',
+      white: '#FFFFFF',
+      icon: '#141423',
+      card: '#151521',
+    }
+  },
+  plugins: [],
+}
+
